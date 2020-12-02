39727c6d10fcd38f5f9e4d762c51085f88d7f545
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2020-12-02 11:20:19 +0800

    Create musicPlayer.js

---
+++ tabs/musicPlayer.js
@@ -0,0 +1,38 @@
+function MusicRandomPlay(address){
+		document.getElementById("musicPlayer").src = address;
+	}
+	
+function RandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min) + min); 
+		//The maximum is exclusive and the minimum is inclusive
+	}
+var a = RandomInt(1,4);
+switch(a){
+	
+	case 1:
+		MusicRandomPlay("//music.163.com/outchain/player?type=2&id=1466303986&auto=1&height=66");
+	//the sixth sense
+		
+		break;
+	
+	case 2:
+		MusicRandomPlay("//music.163.com/outchain/player?type=2&id=28941709&auto=1&height=66");
+	//ヒビカセ
+		
+		break;
+	
+	case 3:
+		MusicRandomPlay("//music.163.com/outchain/player?type=2&id=31356410&auto=1&height=66");
+	//Mr.taxi
+		
+		break;
+	
+	default:
+		MusicRandomPlay("//music.163.com/outchain/player?type=2&id=436016471&auto=1&height=66");
+	//宵々古今
+	
+		break;
+	
+	}
